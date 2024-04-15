fdf072f621680cc1977941810c6cc05a7c00e247
Simon <simonharrisonsiena@gmail.com>
2024-04-15 19:58:13 -0400

    plot update

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 from datetime import datetime
+import pandas as pd
 
 trackedLinks = { #format: file as key, link as value
-    '3090.csv': 'https://www.ebay.ca/sch/i.html?_nkw=rtx+3090&Brand=&_dcat=27386&LH_BIN=1&rt=nc&LH_ItemCondition=1500%7C2010%7C2020%7C1000',
-    '3080.csv': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=rtx+3080&_sacat=0&LH_BIN=1&rt=nc&LH_ItemCondition=1000%7C1500%7C2010',
-    'fenderStratocaster.csv': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=fender+stratocaster&_sacat=0',
-    'yeezy350.csv': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=yeezy+boost+350+zebra&_sacat=15709&US%2520Shoe%2520Size=9&_dcat=15709',
-    'air max 270.csv': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=nike+air+max+270&_sacat=15709&LH_ItemCondition=1000&rt=nc&LH_BIN=1&US%2520Shoe%2520Size=9&_dcat=15709'
+    '3090': 'https://www.ebay.ca/sch/i.html?_nkw=rtx+3090&Brand=&_dcat=27386&LH_BIN=1&rt=nc&LH_ItemCondition=1500%7C2010%7C2020%7C1000&_ipg=60',
+    #'3080': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=rtx+3080&_sacat=0&LH_BIN=1&rt=nc&LH_ItemCondition=1000%7C1500%7C2010',
+    #'fenderStratocaster': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=fender+stratocaster&_sacat=0',
+    #'yeezy350': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=yeezy+boost+350+zebra&_sacat=15709&US%2520Shoe%2520Size=9&_dcat=15709',
+    #'air max 270': 'https://www.ebay.ca/sch/i.html?_from=R40&_nkw=nike+air+max+270&_sacat=15709&LH_ItemCondition=1000&rt=nc&LH_BIN=1&US%2520Shoe%2520Size=9&_dcat=15709'
 }
 
 def getPricesByLink(link):
@@ -38,18 +39,32 @@ def getAverage(prices):
     return np.mean(prices)
 
 def saveToFile(prices, file: str):
-    fields = [datetime.today().strftime('%B-%D-%Y'), datetime.now().strftime('%H:%M:%S'), np.around(getAverage(prices), 2)]
+    fields = [datetime.today().strftime(), datetime.now().strftime('%H:%M:%S'), np.around(getAverage(prices), 2)]
     print('Average Price: ' + str(np.around(getAverage(prices))) + '\n')
-    with open(file, 'a', newline = '') as f:
+    with open(file + '.csv', 'a', newline = '') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
 
-def recordPrice(link: str, file: str):
-    prices = getPricesByLink(link)
+  
+
+for product in trackedLinks.keys():
+    
+    #dfHistory = pd.read_csv(product + '.csv', index_col='date', parse_dates=['date'])
+    
+    
+    print(product)
+    
+    prices = getPricesByLink(trackedLinks[product])
     pricesWithoutOutliers = removeOutliers(prices)
-    saveToFile(pricesWithoutOutliers, file)
     
-for item in trackedLinks.keys():
-    print(item)
-    recordPrice(trackedLinks[item], str(item))
+    listings = pd.Series(pricesWithoutOutliers)
+    listings = listings.sort_values(kind='mergesort', ignore_index=True)
     
+    #saveToFile(pricesWithoutOutliers, product)
+    maximum = listings.max()
+    minimum = listings.min()
+    print(listings)
+    print('max: ' + '%.2f' % maximum + ', min: ' + '%.2f' % minimum + ', mean: ' + '%.2f' % listings.mean())
+    
+    plot = listings.plot(kind='line', title='tempPlot')
+    plt.show()
