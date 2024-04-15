fc22514fb93fdc2cd5db25f39146e55f48067bc0
Simon <simonharrisonsiena@gmail.com>
2024-04-15 00:47:32 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+import csv
+from datetime import datetime
+
+LINK = "https://www.ebay.ca/sch/i.html?_nkw=rtx+3090&Brand=&_dcat=27386&LH_BIN=1&rt=nc&LH_ItemCondition=1500%7C2010%7C2020%7C1000"
+
+def 
