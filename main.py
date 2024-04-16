26d62606d3f588252e52c0a6c53d83e984200f06
Simon <simonharrisonsiena@gmail.com>
2024-04-16 04:24:54 -0400

    stable build update and cleanup

--- main.py
+++ main.py
@@ -2,19 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
 from datetime import datetime
 import pandas as pd
 import json
 from json import loads, dumps
 
-f = open('database.json', 'r')
-parsedDict = json.loads(f.read())
-f.close()
-  
-dictKey = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
-
-d = {
+d = { #starter dictionary for database.json
     "1996retroNuptseBlack": {}, 
      "3090": {}, 
      "3080": {}, 
@@ -22,8 +15,15 @@ d = {
      "yeezy350": {}, 
      "air max 270": {}
 }  
+
+f = open('database.json', 'r')
+parsedDict = json.loads(f.read())
+f.close()
+  
+dictKey = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+
 #parsedDict = d
-    
+
 f = open('tracked-links.json', 'r')
 trackedLinks = json.loads(f.read())
 f.close()
@@ -54,24 +54,10 @@ def removeOutliers(prices, m=2):
 def getAverage(prices):
     return np.mean(prices)
 
-def saveToFile(dataframe): # depreciated
-    
-    fields = [datetime.today().strftime(), datetime.now().strftime('%H:%M:%S'), np.around(getAverage(prices), 2)]
-    print('Average Price: ' + str(np.around(getAverage(prices))) + '\n')
-    with open(file + '.csv', 'a', newline = '') as f:
-        writer = csv.writer(f)
-        writer.writerow(fields)
-
 def writeToDict(product, meanPrice):
-    #parsedDict['date'][dictKey] = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
     parsedDict[product][dictKey] = round(meanPrice, 2)
-    #parsedDict['meanPrice'][dictKey] = 
-
 
 for product in parsedDict.keys():
-    
-    #dfHistory = pd.read_csv(product + '.csv', index_col='date', parse_dates=['date'])
-    
     
     print(product)
     
@@ -81,18 +67,12 @@ for product in parsedDict.keys():
     listings = pd.Series(pricesWithoutOutliers)
     listings = listings.sort_values(kind='mergesort', ignore_index=True)
     
-    #saveToFile(pricesWithoutOutliers, product)
     maximum = listings.max()
     minimum = listings.min()
     
     writeToDict(product, listings.mean())
-    
-    #print(listings)
-    #print('max: ' + '%.2f' % maximum + ', min: ' + '%.2f' % minimum + ', mean: ' + '%.2f' % listings.mean())
-    #histogram(listings, product)
     masterFrame.insert(0, product, listings)
     
-
 print(masterFrame)
 masterFrame.plot.hist(bins=30, alpha=0.5)
 plt.show()
@@ -105,7 +85,6 @@ print(saveData)
 
 saveData.plot(subplots=True)
 plt.show()
-
 
 s = open('database.json', 'w')
 result = saveData.to_json()
