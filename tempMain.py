aeac836f8ef91cfef42493e3d5f0a66b7e8598ad
Simon Siena <simonharrisonsiena@gmail.com>
2024-09-24 09:10:24 -0400

    add virtual environment and clean whitespace

--- tempMain.py
+++ tempMain.py
@@ -10,11 +10,9 @@ import json
 database_file = 'database.csv'
  
 try:
-    # Attempt to read the existing CSV file
     database = pd.read_csv(database_file)
     print('case 1')
 except ValueError:
-    # If the CSV file does not exist, create an empty DataFrame
     database = pd.DataFrame(columns=['Date', 'Item', 'AveragePrice'])
     print('case 2')
 
@@ -22,8 +20,6 @@ items = database['Item'].unique().tolist()
 print('ITEMS: ' + str(items))
 print('DATABASE LOADED: ' + str(database))
 
-
-#initialize master_frame
 master_frame = pd.DataFrame(columns=items)
 print(master_frame)
 
@@ -32,9 +28,7 @@ with open('tracked-links.json', 'r') as f:
 
 def getPricesByLink(link: str) -> list: # Returns list of prices scraped from a given ebay link
     r = requests.get(link)
-    # Parse HTML
     pageParse = BeautifulSoup(r.text, "html.parser")
-    # Narrow list items
     searchResults = pageParse.find('ul', {'class': 'srp-results'}).find_all("li", {'class': 's-item'})
     
     itemPrices = []
@@ -43,7 +37,7 @@ def getPricesByLink(link: str) -> list: # Returns list of prices scraped from a 
         priceAsText = result.find('span', {'class': 's-item__price'}).text
         if 'to' in priceAsText:
             continue
-        price = float(priceAsText[3:].replace(',', ''))  # Remove commas and convert to float
+        price = float(priceAsText[3:].replace(',', ''))  
         itemPrices.append(price)
     return itemPrices
 
@@ -54,29 +48,21 @@ def removeOutliers(prices: list, m=2) -> list:
 def getAverage(prices: list) -> float:
     return np.mean(prices)
 
-
-
 for item in trackedLinks.keys():
     print(item)
     prices = getPricesByLink(trackedLinks[item])
     listings = pd.Series(removeOutliers(prices))
-    listings.sort_values(kind='mergesort', ignore_index=True)   # Sort listings
+    listings.sort_values(kind='mergesort', ignore_index=True)  
     maximum = listings.max()
     minimum = listings.min()
     averagePrice = getAverage(listings)
     new_row_df = pd.DataFrame([{'Date': datetime.now(), 'Item': item, 'AveragePrice': averagePrice}])
     database = pd.concat([database, new_row_df], ignore_index=True)
-    
     master_frame[item] = listings
     print(master_frame)
-
 print(database)
 
-
-
-# Assuming 'database' is your DataFrame and 'Date' and 'AveragePrice' are the columns you want to plot
-
-plt.figure(figsize=(10, 6))  # Set the figure size
+plt.figure(figsize=(10, 6))  
 
 for item in database['Item'].unique():
     item_df = database[database['Item'] == item]
@@ -88,27 +74,20 @@ date_formatter = mdates.DateFormatter('%Y-%m-%d')
 plt.gca().xaxis.set_major_formatter(date_formatter)
 plt.gcf().autofmt_xdate()
 
-plt.xlabel('Date')  # Set the x-axis label
-plt.ylabel('Average Price')  # Set the y-axis label
-plt.title('Line Graph of Average Prices by Item over Time')  # Set the title
-plt.legend()  # Add a legend
-plt.show()  # Show the plot
-
+plt.xlabel('Date')
+plt.ylabel('Average Price')
+plt.title('Line Graph of Average Prices by Item over Time')
+plt.legend()
+plt.show(lj)
 
 fig, axes = plt.subplots(1, len(database['Item'].unique()))
-# Assuming 'master_frame' is your DataFrame
 for item, axis in zip(master_frame.columns.to_list(), axes):
-
-    # Plot the histogram
     axis.hist(master_frame[item], bins=20, alpha=0.5)
     axis.set_xlabel('Price')
     axis.set_ylabel('# Listings')
     axis.set_title(item)
-    
 plt.show()
-
 
 with open(database_file, 'w', newline='') as f:
     database.to_csv(f, index=False)
     print("Data written to", database_file)
-
